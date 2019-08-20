b96e9e8b59fbec63f8aaffe3a89dfb96bebe3814
Alex Muramoto <amuramoto@apache.org>
2019-08-20 12:58:22 -0700

    some refactoring for readability

--- src/example-layers/trips/index.js
+++ src/example-layers/trips/index.js
@@ -20,50 +20,17 @@ import * as GoogleMapsClient from '@google/maps';
 
 export class TripsLayerExample {
   constructor() {}
-  // static *getLayers(google_map) {
-  //   const builder = new TripsBuilder();
-  //   let trips = builder.getTrips(google_map, this.getMapOptions());    
-
-  //   // Limit how long the demo runs
-  //   for (let current_time = 0; current_time <= 2000; current_time++) {
-  //     yield [
-  //       new TripsLayer({
-  //         id: 'trips-layer',
-  //         data: trips,
-  //         getPath: d => d.segments,
-  //         getColor: d => d.mode === 'driving' ? [239, 126, 35] : [85, 181, 238],
-  //         opacity: 0.6,
-  //         widthMinPixels: 2,
-  //         rounded: true,
-  //         trailLength: 75,
-  //         currentTime: current_time
-  //       })
-  //     ]      
-  //   }    
-  // }
-
   static async *getLayers(google_map) {        
-    const builder = new TripsBuilder(google_map);    
-    let current_time = [];
+    const builder = new TripsBuilder(google_map, this.getMapOptions());    
     let i = 0;
-    let j = 0;    
-
-    builder.getTrips(this.getMapOptions());
-    // while (i<3){       
-      
-    //   data.push(next);
-    //   // console.log(next.value)  
-    //   current_time.push(0);
-    //   i++;    
-    // }        
-    // console.log(data)
-    
-
+    builder.genTrips();
     
     while (i < 5000) {
-      let res = [];
-      builder.chunks.forEach((chunk, index) => {      
-        let layer = new TripsLayer({
+      let layers = [];
+      const chunks = builder.getChunks();
+      chunks.forEach((chunk, index) => {  
+        const current_time = builder.getCurrentTime(index);
+        const layer = new TripsLayer({
           id: 'trips-layer-' + index,
           data: chunk,          
           getPath: d => d.route,  
@@ -72,13 +39,13 @@ export class TripsLayerExample {
           widthMinPixels: 2,
           rounded: true,
           trailLength: 100,
-          currentTime: builder.current_time[index]++
+          currentTime: current_time
         })
-        res.push(layer);
+        layers.push(layer);
       })
       i++
-      console.log(res)
-      yield res;      
+      console.log(layers)
+      yield layers;      
     }
   }
 
@@ -97,15 +64,16 @@ export class TripsLayerExample {
 }
 
 class TripsBuilder {
-  constructor(google_map) {
+  constructor(google_map, map_options) {
     this.google_map = google_map;
+    this.map_options = map_options;
     this.directions_service = new google_map.api.DirectionsService();
     this.chunks = [];
     this.current_time = [];
   }
 
-  async getTrips(map_options) {
-    let trips = this.test(map_options);
+  async genTrips(map_options) {
+    let trips = this.genTripsChunks(this.map_options);
     let next = await trips.next();    
     while (!next.done){
       this.chunks.push(next.value);
@@ -114,10 +82,10 @@ class TripsBuilder {
     }
   }
 
-  async *test(map_options) {
+  async *genTripsChunks(map_options) {
     
-    let places = await this.getPlaces(map_options.center);
-    let endpoints = this.genTripsEndpoints(places);
+    let places = await this.getPlaces();
+    let endpoints = this.getTripsEndpoints(places);
     let trips = [];
 
     while (endpoints.length > 0) {
@@ -139,62 +107,17 @@ class TripsBuilder {
     }
   }
 
-  genTripsEndpoints (places) {
-    let endpoints = [];
-    // Build the set of trips and get directions between endpoints 
-    places.forEach(async (place, index) => {      
-      const origin = {
-        lat: place.geometry.location.lat(),
-        lng: place.geometry.location.lng()
-      };
-      for (let j = index + 1; j < places.length; j++) {
-        const dest = {
-          lat: places[j].geometry.location.lat(),
-          lng: places[j].geometry.location.lng()
-        };        
-        const mode = (Math.random() >= 0.5 ? 'DRIVING': 'WALKING');        
-        endpoints.push({origin: origin, dest: dest, mode: mode});
-      }
-    });
-    return endpoints;
-  }
-
-
-  // Builds trips using Places Library for endpoints and Directions Service for segments
-  // async getTrips(map_options) {
-  //   let trips = [];
-  //   let places = await this.getPlaces(google_map.map, map_options.center);
-   
-  //   // Build the set of trips and get directions between endpoints 
-  //   places.forEach(async (place, index) => {
-  //     const origin = {
-  //       lat: place.geometry.location.lat(),
-  //       lng: place.geometry.location.lng()
-  //     };
-  //     for (let j = index + 1; j < places.length; j++) {
-  //       const dest = {
-  //         lat: places[j].geometry.location.lat(),
-  //         lng: places[j].geometry.location.lng()
-  //       };
-  //       const trip = this.getDirections(origin, dest);
-  //       trips.push(trip);        
-  //     }
-  //   });
-  //   trips = await Promise.all(trips);
-  //   console.log(trips)
-  //   return trips;
-  // }
-
   // Gets Places around the map center to use as trip endpoints
-  async getPlaces(center) {    
-    const places_service = new this.google_map.api.places.PlacesService(this.google_map.map);
-    const OPTIONS = {
+  async getPlaces() {    
+    const center = this.map_options.center;
+    const places_service = new this.google_map.api.places.PlacesService(this.google_map.map);    
+    const options = {
       location: new this.google_map.api.LatLng(center),
       radius: '3000',
       type: ['restaurant']
     };
     const places_request = new Promise((resolve, reject) => {
-      places_service.nearbySearch(OPTIONS, (res, status) => {
+      places_service.nearbySearch(options, (res, status) => {
         if (status === 'OK') {
           resolve(res);
         }
@@ -226,25 +149,36 @@ class TripsBuilder {
     
     });
     return directions;
-    // try {
-    //   directions = await directions;
-    //   return directions;
-    // } catch (e) {
-    //   console.log(e);
-    //   setTimeout(async() => await this.getDirections(trip), 2000)
-      
-    // }
-    
-    
-    // const base_uri = 'http://localhost:1337/directions';
-    // const qs = `origin_lat=${origin[0]}&origin_lng=${origin[1]}
-    //             &dest_lat=${dest[0]}&dest_lng=${dest[1]}`;
-    // let request = fetch(`${base_uri}?${qs}`)
-    // let response = await request;
-    // response = await response.json();
-    // return response;
   }
 
+  /* Utils */
+  getTripsEndpoints (places) {
+    let endpoints = [];
+    // Build the set of trips and get directions between endpoints 
+    places.forEach(async (place, index) => {      
+      const origin = {
+        lat: place.geometry.location.lat(),
+        lng: place.geometry.location.lng()
+      };
+      for (let j = index + 1; j < places.length; j++) {
+        const dest = {
+          lat: places[j].geometry.location.lat(),
+          lng: places[j].geometry.location.lng()
+        };        
+        const mode = (Math.random() >= 0.5 ? 'DRIVING': 'WALKING');        
+        endpoints.push({origin: origin, dest: dest, mode: mode});
+      }
+    });
+    return endpoints;
+  }
+
+  getCurrentTime(index) {
+    return this.current_time[index]++;
+  }
+
+  getChunks() {
+    return this.chunks;
+  } 
 
   formatRoute(route) {
     let timestamp = 0;
